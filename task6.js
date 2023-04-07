b7ea71fdb8b814b02d2d76421ce78ee04d56c662
ghous-mohammad <ghosmohammad247@gmail.com>
2023-04-07 22:32:44 +0500

    corrected task6.js

--- task6.js
+++ task6.js
@@ -4,26 +4,14 @@ for (index = 1; index <= 100; index++) {
   let fizz = index % 3;
   let buzz = index % 5;
 
-  //     if(fizz===0){
-  //     console.log("fizz");
-  //    }
-  // if(buzz==0 && fizz===0){
-  //     console.log("fizzbuzz");
-  //    }else if(buzz===0){console.log("fizz");}
-  switch ((fizz, buzz)) {
-    case 1:
-      buzz == 0;
-      console.log("buzz");
-      break;
-    case 2:
-      fizz == 0;
-      console.log("fizz");
-      break;
-    case 3:
-      buzz == 0 && fizz == 0;
-      console.log("fizz buzz");
-
-    default:
-      break;
+  if (fizz === 0 && buzz === 0) {
+    console.log("FizzBuzz");
+  } else if (fizz % 3 === 0) {
+    console.log("Fizz");
+  } else if (buzz % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(index);
   }
 }
+
